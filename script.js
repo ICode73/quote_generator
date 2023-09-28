7d86ab7da2aea4952e318b4dc94959d12563d80c
ADonovanWP <anthonyrdonovan1@gmail.com>
2023-09-27 20:05:53 -0500

    added functionality to the buttons, one is to post quote on twitter, the other is to update a new quote

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-"use-strict";
+"use strict";
 
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 /*
 api for quote generator:
 https://jacintodesign.github.io/quotes-api/data/quotes.json
@@ -12,7 +17,20 @@ function newQuote() {
   // Pick a random quote from apiQuotes
 
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // to check if author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 
   //   * this is pulling for local storage (newQuote)
   //   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
@@ -31,6 +49,16 @@ async function getQuotes() {
     // Catch Error Here
   }
 }
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
